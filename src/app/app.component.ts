e7a00a9d24938c1125007eeb903f1aefbd6e1595
MoElsherif26 <m.elsherif2026@gmail.com>
2025-04-10 07:11:27 +0200

    Created About and Projects Sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { NavBarComponent } from "./layouts/nav-bar/nav-bar.component";
 import { FlowbiteService } from './core/services/flowbite.service';
 import { initFlowbite } from 'flowbite';
 import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { ProjectsComponent } from "./pages/projects/projects.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, HomeComponent],
+  imports: [NavBarComponent, HomeComponent, AboutComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
