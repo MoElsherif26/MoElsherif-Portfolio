dd7fd25a22e03232dad0ff09acec4514d06f0184
MoElsherif26 <m.elsherif2026@gmail.com>
2025-04-10 03:29:55 +0200

    Created Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./layouts/nav-bar/nav-bar.component";
+import { FlowbiteService } from './core/services/flowbite.service';
+import { initFlowbite } from 'flowbite';
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavBarComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'MoElsherif-Portfolio';
+  constructor(private flowbiteService: FlowbiteService) {}
+  
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
