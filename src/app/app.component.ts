96ff07dad6e5487af14971ebbc3c74a6703fba81
MoElsherif26 <m.elsherif2026@gmail.com>
2025-04-11 02:15:48 +0200

    Added AnimationOnScrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,44 @@
-import { Component } from '@angular/core';
-import { NavBarComponent } from "./layouts/nav-bar/nav-bar.component";
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
 import { FlowbiteService } from './core/services/flowbite.service';
 import { initFlowbite } from 'flowbite';
-import { HomeComponent } from "./pages/home/home.component";
-import { AboutComponent } from "./pages/about/about.component";
-import { ProjectsComponent } from "./pages/projects/projects.component";
-import { FooterComponent } from "./layouts/footer/footer.component";
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, HomeComponent, AboutComponent, ProjectsComponent, FooterComponent],
+  standalone: true,
+  imports: [
+    NavBarComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'MoElsherif-Portfolio';
-  constructor(private flowbiteService: FlowbiteService) {}
-  
+
+  constructor(
+    private flowbiteService: FlowbiteService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
   ngOnInit(): void {
-    this.flowbiteService.loadFlowbite((flowbite) => {
-      initFlowbite();
-    });
+    if (isPlatformBrowser(this.platformId)) {
+      this.flowbiteService.loadFlowbite(() => {
+        initFlowbite();
+      });
+
+      AOS.init({
+        duration: 1000,
+      });
+    }
   }
 }
